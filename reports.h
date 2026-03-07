c388b20641f9540bc01f65d354b548c49a7a0463
Salma-Khaled <salmaakh000@gmail.com>
2026-03-08 00:28:38 +0200

    Update reports.h

--- reports.h
+++ reports.h
@@ -1,3 +1,8 @@
-#include "library.cpp"
+#ifndef REPORTS_H
+#define REPORTS_H
+#include "library.h"
+#include <string>
+
 void generateOverdueReport(const LibraryState& state);
 void exportOverdueBooks(const LibraryState& state, const std::string& filename);
+#endif
