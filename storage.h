5667288304a50bdde198ce708ffb1c511f23bf40
Salma-Khaled <salmaakh000@gmail.com>
2026-03-08 00:14:38 +0200

    Update storage.h

--- storage.h
+++ storage.h
@@ -1,2 +1,8 @@
+#ifndef STORAGE_H
+#define STORAGE_H
+#include "library.h"
+#include <string>
+
 void saveDatabase(const LibraryState& state, const std::string& dbPath);
 void loadDatabase(LibraryState& state, const std::string& dbPath);
+#endif
