a5e5210cfdb52c9034ebab90988eb1646d82a6aa
Salma-Khaled <salmaakh000@gmail.com>
2026-03-11 01:37:11 +0200

    Delete reports.h

--- reports.h
+++
@@ -1,8 +0,0 @@
-#ifndef REPORTS_H
-#define REPORTS_H
-#include "library.h"
-#include <string>
-
-void generateOverdueReport(const LibraryState& state);
-void exportOverdueBooks(const LibraryState& state, const std::string& filename);
-#endif
