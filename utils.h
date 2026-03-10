501e417782ac7213752cb6dfaa6d84c2330980ff
Salma-Khaled <salmaakh000@gmail.com>
2026-03-11 01:39:12 +0200

    Update utils.h

--- utils.h
+++ utils.h
@@ -1,8 +1,3 @@
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define BLUE    "\033[34m"
-#define RESET   "\033[0m"
-
 #ifndef UTILS_H
 #define UTILS_H
 
@@ -36,11 +31,11 @@ inline void logActivity(const string& message) {
         time_t now = time(0);
         char dt[26];
 
-        // ctime_s دي النسخة اللي Visual Studio بيحبها عشان الأمان
+       
         ctime_s(dt, sizeof(dt), &now);
 
         string timeStr(dt);
-        if(!timeStr.empty()) timeStr.pop_back(); // بنشيل السطر الزيادة اللي بيجي مع الوقت
+        if(!timeStr.empty()) timeStr.pop_back(); 
 
 
         ctime_s(dt, sizeof(dt), &now);
@@ -52,3 +47,5 @@ inline void logActivity(const string& message) {
 }
 
 #endif
+
+
